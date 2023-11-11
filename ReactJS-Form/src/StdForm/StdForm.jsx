e891f9be3c55fc4c2806ae25fca803ebf566d07c
QuangTruong <nqtruong2210@gmail.com>
2023-11-12 03:24:53 +0700

    update code

--- ReactJS-Form/src/StdForm/StdForm.jsx
+++ ReactJS-Form/src/StdForm/StdForm.jsx
@@ -10,6 +10,13 @@ export const StdForm = () => {
     email: "",
   });
 
+  const [formError, setFormError] = useState({
+    id: "",
+    fullName: "",
+    phoneNumber: "",
+    email: "",
+  });
+
   const { stdEdit } = useSelector((state) => state.exForm);
 
   useEffect(() => {
@@ -23,6 +30,37 @@ export const StdForm = () => {
 
   const { prdEdit } = useSelector((state) => state.exForm);
   console.log("prdEdit: ", prdEdit);
+
+  const validation = (name, value) => {
+    switch (name) {
+      case "id":
+        if (value === "") {
+          return "Vui lòng nhập thông tin";
+        } else {
+          return "";
+        }
+      case "phoneNumber":
+        if (value === "") {
+          return "Vui lòng nhập thông tin";
+        } else {
+          return "";
+        }
+      case "fullName":
+        if (value === "") {
+          return "Vui lòng nhập thông tin";
+        } else {
+          return "";
+        }
+      case "email":
+        if (value === "") {
+          return "Vui lòng nhập thông tin";
+        } else {
+          return "";
+        }
+      default:
+        return "";
+    }
+  };
   // Currying Function
   const handleFormValue = (name) => (e) => {
     setFormValue({
@@ -30,6 +68,10 @@ export const StdForm = () => {
       [name]: e.target.value,
     });
     // console.log(e.target.value);
+    setFormError({
+      ...formError,
+      [name]: validation(name, event.target.value),
+    });
   };
 
   return (
@@ -37,6 +79,32 @@ export const StdForm = () => {
       className="row"
       onSubmit={(e) => {
         e.preventDefault();
+        const validationError = {};
+
+        Object.keys(formValue).forEach((name) => {
+          const error = validation(name, formValue[name]);
+          console.log("formValue[name]: ", formValue[name]);
+
+          if (error && error.length > 0) {
+            validationError[name] = error;
+          }
+        });
+        if (Object.keys(validationError).length > 0) {
+          setFormError({ ...validationError });
+          return;
+        }
+        if (stdEdit) {
+          dispatch(exFormActions.editStd(formValue));
+        } else {
+          dispatch(exFormActions.createStd(formValue));
+        }
+
+        setFormValue({
+          id: "",
+          fullName: "",
+          phoneNumber: "",
+          email: "",
+        });
       }}
     >
       <p className="bg-dark text-white p-2 fw-bold fs-4 fst-italic">
@@ -52,6 +120,11 @@ export const StdForm = () => {
             onChange={handleFormValue("id")}
             disabled={formValue.id === stdEdit?.id}
           />
+          {formError.id && (
+            <p>
+              <small className="text-danger">{formError.id}</small>
+            </p>
+          )}
         </div>
         <div>
           <p className="fw-bold mt-3">PHONE NUMBER:</p>
@@ -61,6 +134,11 @@ export const StdForm = () => {
             value={formValue.phoneNumber}
             onChange={handleFormValue("phoneNumber")}
           />
+          {formError.phoneNumber && (
+            <p>
+              <small className="text-danger">{formError.phoneNumber}</small>
+            </p>
+          )}
         </div>
       </div>
       <div className="col-6">
@@ -72,6 +150,11 @@ export const StdForm = () => {
             value={formValue.fullName}
             onChange={handleFormValue("fullName")}
           />
+          {formError.fullName && (
+            <p>
+              <small className="text-danger">{formError.fullName}</small>
+            </p>
+          )}
         </div>
         <div>
           <p className="fw-bold mt-3">EMAIL:</p>
@@ -81,38 +164,43 @@ export const StdForm = () => {
             value={formValue.email}
             onChange={handleFormValue("email")}
           />
+          {formError.email && (
+            <p>
+              <small className="text-danger">{formError.email}</small>
+            </p>
+          )}
         </div>
       </div>
       <div className="mt-3">
         {stdEdit ? (
           <button
             className="btn btn-primary"
-            onClick={() => {
-              if (stdEdit) {
-                dispatch(exFormActions.editStd(formValue));
-                setFormValue({
-                  id: "",
-                  fullName: "",
-                  phoneNumber: "",
-                  email: "",
-                });
-              }
-            }}
+            // onClick={() => {
+            //   if (stdEdit) {
+            //     dispatch(exFormActions.editStd(formValue));
+            //     setFormValue({
+            //       id: "",
+            //       fullName: "",
+            //       phoneNumber: "",
+            //       email: "",
+            //     });
+            //   }
+            // }}
           >
             <i className="fa-solid fa-upload" />
           </button>
         ) : (
           <button
             className="btn btn-success"
-            onClick={() => {
-              dispatch(exFormActions.createStd(formValue));
-              setFormValue({
-                id: "",
-                fullName: "",
-                phoneNumber: "",
-                email: "",
-              });
-            }}
+            // onClick={() => {
+            //   dispatch(exFormActions.createStd(formValue));
+            //   setFormValue({
+            //     id: "",
+            //     fullName: "",
+            //     phoneNumber: "",
+            //     email: "",
+            //   });
+            // }}
           >
             <i className="fa-solid fa-plus" />
           </button>
